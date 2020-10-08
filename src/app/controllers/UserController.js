e7a20c831e0c89bf2062c7590a6e3044afef67b1
INDRA\dsenam <dsenam@indracompany.com>
2020-10-07 21:05:36 -0300

    feat: update de usuário

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -19,6 +19,28 @@ class UserController {
             login,
         });
     }
+
+    async update(req, res) {
+        const { login, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (login !== user.login) {
+            const userExists = await User.findOne({ where: { login } });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'Usuário já existe' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ error: 'Password does not match' });
+        }
+
+        const { id } = await user.update(req.body);
+
+        return res.json({ id, login });
+    }
 }
 
 export default new UserController();
