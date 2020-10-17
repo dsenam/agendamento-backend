96206390d38f00f6a32a6a19b08e55e068943b39
INDRA\dsenam <dsenam@indracompany.com>
2020-10-16 23:47:18 -0300

    listagem disponiveis

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import AppointmentController from './app/controllers/AppointmentController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMiddleware from './app/middleware/auth';
 
@@ -15,6 +16,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.get('/available', AvailableController.index);
 
 routes.post('/appointments', AppointmentController.store);
 
